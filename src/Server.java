6fd93b7444cba0edb9f0dce1f8832ce47d453a13
unknown <gabriel.laprise@gmail.com>
2013-09-20 14:07:00 -0400

    added handling for Connection refused in Server

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -24,65 +25,90 @@ public class Server
 		Socket socket;
 		Process process;
 		EmailSender sender = new EmailSender();
-
-		try {
+		
+		process = createProcess();
+		
+		while(true)
+		{
+			
+			try {
+				Thread.sleep(5*1000);
+				
+			    while(true)
+				{
+					socket = new Socket("127.0.0.1", 49152);
+					
+					BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			        String answer = input.readLine();
+			        
+			        System.out.println(answer);
+			        
+			        if(!Boolean.valueOf(answer))
+			        {
+			        	System.out.println("Killing Process");
+			        			        	
+			        	socket.close();
+			        	
+			        	process.destroy();
+			        	
+			        	System.out.println("Restarting Process");
+			        	
+			        	process = createProcess();
+			        	
+			        	sender.Send("gabriel.laprise", "GAla25!!", "gabriel.laprise@outlook.com","Process Died", "Process restarted ");
+			        }
+			        else
+			        {
+			        	socket.close();
+			        }
+	
+			        
+			        Thread.sleep(5*1000);
+				}
+				
+			} 
+			catch (ConnectException e) 
+			{
+				System.out.println("Catched ConnectionException");
+			
+			}
+		    catch (UnknownHostException e) 
+			{
+				e.printStackTrace();
+			} 
+		     catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			} catch (AddressException e) {
+				
+				e.printStackTrace();
+			} catch (MessagingException e) {
+				
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static Process createProcess()
+	{
+		try
+		{
+			Process process;
 			
 			//Create Process
 			ProcessBuilder procesBuilder = new ProcessBuilder("java","-jar","dist/Client.jar","49152");
 			process = procesBuilder.start();
 			
-			Thread.sleep(5*1000);
-			
-		    while(true)
-			{
-				socket = new Socket("127.0.0.1", 49152);
-				
-				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		        String answer = input.readLine();
-		        
-		        System.out.println(answer);
-		        
-		        if(!Boolean.valueOf(answer))
-		        {
-		        	System.out.println("Killing Process");
-		        			        	
-		        	socket.close();
-		        	
-		        	process.destroy();
-		        	
-		        	System.out.println("Restarting Process");
-		        	
-		        	process = procesBuilder.start();
-		        	
-		        	sender.Send("gabriel.laprise", "GAla25!!", "gabriel.laprise@outlook.com","Process Died", "Process restarted ");
-		        }
-		        else
-		        {
-		        	socket.close();
-		        }
-
-		        
-		        Thread.sleep(5*1000);
-			}
-			
-		} 
-		catch (UnknownHostException e) 
-		{
-			e.printStackTrace();
-		} 
+			return process;
+		}	
 		catch (IOException e) 
 		{
 			e.printStackTrace();
-		} catch (InterruptedException e) {
-			
-			e.printStackTrace();
-		} catch (AddressException e) {
-			
-			e.printStackTrace();
-		} catch (MessagingException e) {
-			
-			e.printStackTrace();
 		}
-
+		
+		return null;
 	}
 }
